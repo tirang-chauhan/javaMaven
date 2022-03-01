7cd237e51bdec81df9cc103b2bcbc11c70c60aad
Tirang Chauhan <tirangchauhan9958.tc@gmail.com>
2022-03-01 16:12:44 +0530

    first commit

---
+++ src/test/java/com/company/AppTest.java
@@ -0,0 +1,12 @@
+package com.company;
+import org.junit.Assert.*;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class AppTest {
+@Test
+    public void test(){
+
+    System.out.println("Hello world");
+    }
+}
